c1a7f49640f03ef882876a528b540e0ea8f54681
Schondaniel <76953235+Schondaniel@users.noreply.github.com>
2021-11-08 14:35:44 +0100

    Add files via upload

---
+++ analyze.c
@@ -0,0 +1,61 @@
+#include "analyze.h"
+#include "algorithm.h"
+
+//
+// Private
+//
+
+//
+// Public
+//
+void benchmark(const algorithm_t a, const case_t c, result_t *buf, int n)
+{
+    switch (a) {
+        case 'bubble_sort_t':
+            switch (c) {
+                case 'best_t':
+                  
+                case 'worst-t':
+
+                case 'average_t':
+            break;
+            }
+        
+        case 'insertion_sort_t':
+            switch (c) {
+                case 'best_t':
+                  
+                case 'worst-t':
+
+                case 'average_t':
+            }
+
+        case 'quick_sort_t':
+            switch (c) {
+                case 'best_t':
+                  
+                case 'worst-t':
+
+                case 'average_t':
+            }
+
+        case 'linear_search_t':
+            switch (c) {
+                case 'best_t':
+                  
+                case 'worst-t':
+
+                case 'average_t':
+            }
+        
+        case 'binary_search_t':
+            switch (c) {
+                case 'best_t':
+                  
+                case 'worst-t':
+
+                case 'average_t':
+            }
+    }
+
+}
